59e475b822c320f36bcae0e2e9ebad9d964dd7ec
Andrew Brown <ab36245@gmail.com>
2025-06-15 17:50:22 +1000

    Mintor tweak to MsgDecoder

--- lib/src/decoder.dart
+++ lib/src/decoder.dart
@@ -5,13 +5,13 @@ import 'package:flutter_msgpack/flutter_msgpack.dart';
 import 'decoders/object.dart';
 
 class MsgDecoder extends ObjectDecoder {
-  factory MsgDecoder(Uint8List b) {
-    final mp = MsgPackDecoder(b);
+  factory MsgDecoder(Uint8List bytes) {
+    final mp = MsgPackDecoder(bytes);
     final id = mp.getInt();
     return MsgDecoder._(id, mp);
   }
 
-  MsgDecoder._(this.id, super._mp);
-
   final int id;
+
+  MsgDecoder._(this.id, super._mp);
 }
